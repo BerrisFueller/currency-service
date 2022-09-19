e5c66c0ebd1ddb5d34327e46bb877f01ea60f2b8
Melisa <melcafidan@gmail.com>
2022-09-19 20:17:53 +0200

    fix setup and change price data type to BigDecimal

--- src/main/java/com/example/currencyservice/consumer/CurrencyConsumer.java
+++ src/main/java/com/example/currencyservice/consumer/CurrencyConsumer.java
@@ -2,11 +2,15 @@ package com.example.currencyservice.consumer;
 
 import com.example.currencyservice.Currency;
 import com.example.currencyservice.consumer.dto.CurrencyRequest;
-import com.example.currencyservice.consumer.dto.CurrencyResponse;
 import com.example.currencyservice.service.CurrencyService;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 
 
 @Slf4j
@@ -15,20 +19,22 @@ public class CurrencyConsumer {
     @Autowired
     CurrencyService currencyService;
 
-    int receivedPrice;
+    BigDecimal receivedPrice;
     Currency wantedCurrency;
 
     @RabbitListener(queues = "${queue.currency}")
-    public CurrencyResponse ConversionOfCurrency(CurrencyRequest currencyRequest){
+    public String ConversionOfCurrency(Message message) {
 
+        CurrencyRequest currencyRequest = new Gson()
+                .fromJson(new String(message.getBody(), StandardCharsets.UTF_8), CurrencyRequest.class);
 
-    try {
-        receivedPrice = currencyRequest.getPrice();
-        wantedCurrency = currencyRequest.getWantedCurrency();
-    }catch (IllegalArgumentException e){
-        log.error("Illegal REQUEST!" + e);
-    }
-       return currencyService.updatePriceToSelectedCurrency(receivedPrice,wantedCurrency);
+        try {
+            receivedPrice = currencyRequest.getPrice();
+            wantedCurrency = currencyRequest.getWantedCurrency();
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal REQUEST!" + e);
+        }
+        return new Gson().toJson(currencyService.updatePriceToSelectedCurrency(receivedPrice, wantedCurrency));
     }
 
 }
