9e7d1f6a3662df1bcf20e73b92c5f16759ff6b15
NasiemZ <nasiem2@hotmail.de>
2022-09-28 19:28:27 +0200

    Added Some JUNIT Tests

--- src/test/java/com/example/currencyservice/CurrencyServiceApplicationTests.java
+++ src/test/java/com/example/currencyservice/CurrencyServiceApplicationTests.java
@@ -17,42 +17,62 @@ class CurrencyServiceApplicationTests {
     private final CurrencyService currencyService = new CurrencyService();
 
     @Test
-    public void InEUR(){
+    public void inEUR() {
 
-        CurrencyResponse currencyResponse =currencyService.updatePriceToSelectedCurrency(BigDecimal.valueOf(100),Currency.EUR);
-        assertEquals(BigDecimal.valueOf(100).multiply(BigDecimal.valueOf(1.2)).setScale(2, RoundingMode.CEILING),currencyResponse.getPrice());
+        CurrencyResponse currencyResponse = currencyService.
+                updatePriceToSelectedCurrency(BigDecimal.valueOf(100), Currency.EUR);
+
+        assertEquals(BigDecimal.valueOf(100)
+                .multiply(BigDecimal.valueOf(1.2))
+                .setScale(2, RoundingMode.CEILING), currencyResponse.getPrice());
     }
 
     @Test
-    public void InPhp(){
+    public void inPhp() {
 
 
-        CurrencyResponse currencyResponse =currencyService.updatePriceToSelectedCurrency(BigDecimal.valueOf(100),Currency.PHP);
-        assertEquals(BigDecimal.valueOf(100).multiply(BigDecimal.valueOf(52.44)).setScale(2, RoundingMode.CEILING),currencyResponse.getPrice());
+        CurrencyResponse currencyResponse = currencyService
+                .updatePriceToSelectedCurrency(BigDecimal.valueOf(100), Currency.PHP);
+
+        assertEquals(BigDecimal.valueOf(100).
+                multiply(BigDecimal.valueOf(52.44)).
+                setScale(2, RoundingMode.CEILING), currencyResponse.getPrice());
     }
 
     @Test
-    public void InIsk(){
+    public void inIsk() {
 
 
-        CurrencyResponse currencyResponse =currencyService.updatePriceToSelectedCurrency(BigDecimal.valueOf(100),Currency.ISK);
-        assertEquals(BigDecimal.valueOf(100).multiply(BigDecimal.valueOf(138.85)).setScale(2, RoundingMode.CEILING),currencyResponse.getPrice());
+        CurrencyResponse currencyResponse = currencyService.
+                updatePriceToSelectedCurrency(BigDecimal.valueOf(100), Currency.ISK);
+
+        assertEquals(BigDecimal.valueOf(100)
+                .multiply(BigDecimal.valueOf(138.85))
+                .setScale(2, RoundingMode.CEILING), currencyResponse.getPrice());
     }
 
     @Test
-    public void InYen(){
+    public void inYen() {
 
 
-        CurrencyResponse currencyResponse =currencyService.updatePriceToSelectedCurrency(BigDecimal.valueOf(100),Currency.YEN);
-        assertEquals(BigDecimal.valueOf(100).multiply(BigDecimal.valueOf(130.5)).setScale(2, RoundingMode.CEILING),currencyResponse.getPrice());
+        CurrencyResponse currencyResponse = currencyService.
+                updatePriceToSelectedCurrency(BigDecimal.valueOf(100), Currency.YEN);
+
+        assertEquals(BigDecimal.valueOf(100)
+                .multiply(BigDecimal.valueOf(130.5))
+                .setScale(2, RoundingMode.CEILING), currencyResponse.getPrice());
     }
 
     @Test
-    public void InUSD(){
+    public void inUSD() {
 
 
-        CurrencyResponse currencyResponse =currencyService.updatePriceToSelectedCurrency(BigDecimal.valueOf(120),Currency.USD);
-        assertEquals(BigDecimal.valueOf(1).multiply(BigDecimal.valueOf(120)).setScale(2, RoundingMode.CEILING),currencyResponse.getPrice());
+        CurrencyResponse currencyResponse = currencyService
+                .updatePriceToSelectedCurrency(BigDecimal.valueOf(120), Currency.USD);
+
+        assertEquals(BigDecimal.valueOf(1)
+                .multiply(BigDecimal.valueOf(120))
+                .setScale(2, RoundingMode.CEILING), currencyResponse.getPrice());
     }
 
 }
